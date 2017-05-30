4d409e7b9afef2db760018306587974a3966906e
HSpor <hsporys@hotmail.de>
2017-05-30 14:37:54 +0200

    Arc erstellt

--- src/assignment3_DS/Arc.java
+++ src/assignment3_DS/Arc.java
@@ -1,20 +1,49 @@
 package assignment3_DS;
 
 /**
- * Created by pv42 on 29.05.2017.
+ * An arc is the connection between 2 nodes.
+ * It contains the distance between the connected Nodes
+ * Created on 29.05.2017.
  */
 public class Arc {
+    private Node startNode, endNode;
+    private int distance;
+
     /**
-     * Return a arcs length e.g. distance which must be overcome to reach the
+     * Creates the new object Arc
+     * @param startNode
+     * @param endNode
+     * @param distance
+     */
+    public  Arc(Node startNode, Node endNode, int distance) {
+        this.startNode = startNode;
+        this.endNode = endNode;
+        this.distance = distance;
+    }
+
+    /**
+     * Return an arcs length e.g. distance which must be overcome to reach the
      * @return the distance from start to end
      */
     public int getDistance() {
-        return 0;
+        return distance;
     }
+
+    /**
+     * Returns the start of an arc (as Node)
+     * @return the startnode
+     */
     public Node getStart() {
-        return null;
+        return startNode;
+
     }
+
+    /**
+     * Returns the end of an arc (as Node)
+     * @return the endnode
+     */
     public  Node getEnd() {
-        return null;
+        return endNode;
+
     }
 }
