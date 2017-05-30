a0a168e26e257eff3c0b94ffb32359f9d3b06ec7
HSpor <hsporys@hotmail.de>
2017-05-30 15:10:19 +0200

    currentspeedmod...

--- src/assignment3_DS/Node.java
+++ src/assignment3_DS/Node.java
@@ -1,5 +1,7 @@
 package assignment3_DS;
 
+import java.util.Objects;
+
 /**
  * Created by pv42 on 29.05.2017.
  * Represnets a Node of the
@@ -8,7 +10,11 @@ public class Node {
 
     private int id;
     private String mod;
-
+    /**
+     * create a Node
+     * @param id
+     * @param mod
+     */
     public Node(int id, String mod) {
         this.id = id;
         this.mod = mod;
@@ -20,6 +26,11 @@ public class Node {
      * @return speed when leaving this node
      */
     public double applySpeedModifier(double currentSpeed) {
+        if (Objects.equals(mod, "-")) return currentSpeed;
+        if (Objects.equals(mod, "*speed")) return currentSpeed * currentSpeed;
+        if (mod.substring(0,1).equals("+")) return currentSpeed + Double.valueOf(mod.substring(1));
+        if (mod.substring(0,1).equals("-")) return currentSpeed - Double.valueOf(mod.substring(1));
+        if (mod.substring(0,1).equals("*")) return currentSpeed * Double.valueOf(mod.substring(1));
         return 0;
     }
 
@@ -27,7 +38,10 @@ public class Node {
      * Returns the nodes identifier(ID)
      * @return the nodes ID
      */
+
     public int getID() {
-        return 0;
+        return this.id;
     }
+
+
 }
