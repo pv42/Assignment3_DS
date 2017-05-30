bb5fed65c4182104758413818b8de66757fb9986
pv42 <pv42.97@gmail.com>
2017-05-30 17:32:42 +0200

    added needed methodes in path

--- src/assignment3_DS/Path.java
+++ src/assignment3_DS/Path.java
@@ -4,6 +4,16 @@ package assignment3_DS;
  * Created by pv42 on 29.05.2017.
  */
 public class Path {
+
+    public void addArc() {
+
+    }
+    public double getTimeNeeded() { //default : startspeed = 1
+        return 0;
+    }
+    public double getTimeNeeded(double startSpeed) {
+        return 0;
+    }
     @Override
     public String toString() {
         return "Path{}";
