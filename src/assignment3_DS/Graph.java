61e70ffeefa3ed2346acc13f8a179d0b3673df28
pv42 <pv42.97@gmail.com>
2017-05-30 18:54:01 +0200

    ...

--- src/assignment3_DS/Graph.java
+++ src/assignment3_DS/Graph.java
@@ -8,7 +8,7 @@ import java.util.List;
 public class Graph {
 
     public Graph(List<Node> nodes, List<Arc> arcs) {
-        System.out.print(nodes.toString() + arcs.toString());
+        System.out.print(nodes.toString() + arcs.toString()); //remove
         //...
     }
 
