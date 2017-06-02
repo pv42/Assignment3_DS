2c7760af7cefec62c72dbb44036c75b7f503eaed
Sebastian <sebastian94.nolte@googlemail.com>
2017-06-02 20:58:34 +0200

    Changes

--- src/assignment3_DS/Path.java
+++ src/assignment3_DS/Path.java
@@ -13,27 +13,51 @@ public class Path {
 
     private List <Arc> arcList;
 
-    // initialisiert neue Liste
+    /**
+     * Creates Path
+     *
+     */
     public Path(){
         arcList = new ArrayList<>();
     }
 
-    public Path(Path path) {}
+    public Path(Path path) {
 
-    // greifen auf letztes Element in der Liste zu
+    }
+
+    /**
+     * Returns the last Node in arcList
+     *
+     * @return last Node
+     */
     public Node getEndNode() {
         return arcList.get(arcList.size()-1).getEnd();
     }
 
-    // fuegen neue Arc/Kante hinzu
+    /**
+     * Adds a new Arc to arcList
+     *
+     * @param arc Adds arc
+     */
     public void addArc(Arc arc) {
         arcList.add(arc);
     }
 
+    /**
+     * Calculates the total time needed to finish the path once
+     *
+     * @return total Time needed to finish path
+     */
     public double getTimeNeeded() { //default : startspeed = 1;
         return getTimeNeeded(DEFAULT_START_SPEED);
     }
 
+    /**
+     * Calculates the total time needed to finish the path once
+     *
+     * @param startSpeed startspeed is equal DEFAULT_START_SPEED
+     * @return total Time needed to finish path
+     */
     public double getTimeNeeded(double startSpeed) {
             int distance = 0;
             double speed = startSpeed;
@@ -46,10 +70,21 @@ public class Path {
             return totalTime;
     }
 
+    /**
+     *  Calculates the end speed the path reaches after one cycle
+     *
+     * @return total speed after one cycle
+     */
     public double getEndSpeed() { //default : startspeed = 1;
         return getEndSpeed(DEFAULT_START_SPEED);
     }
 
+    /**
+     *  Calculates the end speed the path reaches after one cycle
+     *
+     * @param startSpeed startspeed of node
+     * @return total speed after one cycle
+     */
     public double getEndSpeed(double startSpeed) {
         double speed = startSpeed;
         for (int i = 0; i < arcList.size(); i++){
@@ -58,6 +93,11 @@ public class Path {
         return speed;
     }
 
+    /**
+     *
+     * @param path
+     * @return
+     */
     public boolean isBetterThan(Path path) {
         return getEndSpeed() >= path.getEndSpeed() && getTimeNeeded() <= path.getTimeNeeded();
     }
@@ -67,7 +107,9 @@ public class Path {
         String out = "";
         for (int i = 0; i < arcList.size(); i++){
             out = out + arcList.get(i).getStart();
-            if (i < (arcList.size()-1)){ out = out + ",";}
+            if (i < (arcList.size()-1)){
+                out = out + ",";
+            }
         }
         return "Path{" + out + "}";
     }
