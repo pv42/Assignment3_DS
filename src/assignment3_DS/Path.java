3ea4282ecd878d2d2860798af82d5f45f5ff73fd
Sebastian <sebastian94.nolte@googlemail.com>
2017-06-02 17:57:41 +0200

    ...

--- src/assignment3_DS/Path.java
+++ src/assignment3_DS/Path.java
@@ -1,23 +1,38 @@
 package assignment3_DS;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by pv42 on 29.05.2017.
- * @author Sebastian Nullte
+ * @author Sebastian Nolte
  */
 public class Path {
     private static final double DEFAULT_START_SPEED = 1;
 
-    public Path(){}
+    private List <Arc> arcList;
+
+    // initialisiert neue Liste
+    public Path(){
+        List<Arc> path = new ArrayList<>();
+    }
 
     public Path(Path path) {}
 
+    // greifen auf letztes Element in der Liste zu
     public Node getEndNode() {
-        return null;
+        return arcList.get(arcList.size()-1).getEnd();
     }
 
-    public void addArc(Arc arc) {}
+    // fuegen neue Arc/Kante hinzu
+    public void addArc(Arc arc) {
+        arcList.add(arc);
+    }
 
-    public double getTimeNeeded() { //default : startspeed = 1
+    public double getTimeNeeded() { //default : startspeed = 1;
+
+        // time = getDistance() / (applySpeedModifier();
+        // zeit = weg / geschwindigkeit
         return getTimeNeeded(DEFAULT_START_SPEED);
     }
 
@@ -25,7 +40,8 @@ public class Path {
         return 0;
     }
 
-    public double getEndSpeed() {
+    public double getEndSpeed() { //default : startspeed = 1;
+        // hier muss ich die gesamtzeit erfassen, also alle zeiten zusammenrechnen
         return getEndSpeed(DEFAULT_START_SPEED);
     }
 
@@ -41,4 +57,5 @@ public class Path {
     public String toString() {
         return "Path{}";
     }
+
 }
