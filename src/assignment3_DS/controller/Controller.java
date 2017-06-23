3990293a73587e53ee2b6519d77dcf24bd955cd1
Jörn Roth <pv42.97@gmail.com>
2017-06-23 15:37:55 +0200

    Update Controller.java
    corrected some return trypes, added get to some names

--- src/assignment3_DS/controller/Controller.java
+++ src/assignment3_DS/controller/Controller.java
@@ -4,18 +4,18 @@ package assignment3_DS.controller;
  * Created on 23.06.2017.
  */
 public class Controller {
-
+    // make all this functions static ?
     public void removeArcsGreaterThen(int Weight);
 
-    public int nodeCount();
+    public int getNodeCount();
 
-    public int arcCount();
+    public int getArcCount();
 
-    public int arcWeightSum();
+    public int getArcWeightSum();
 
-    public String nodeList();
+    public List<String> getNodeList();
 
-    public String operationList();
+    public List<String> getOperationList();
 
 }
 
