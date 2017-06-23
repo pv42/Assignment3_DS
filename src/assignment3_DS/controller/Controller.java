1e9e39569568fa7a71bafb9a0aacc5fccf3d9f1e
HSpor <hsporys@hotmail.de>
2017-06-23 15:24:26 +0200

    layout changes

---
+++ src/assignment3_DS/controller/Controller.java
@@ -0,0 +1,21 @@
+package assignment3_DS.controller;
+
+/**
+ * Created on 23.06.2017.
+ */
+public class Controller {
+
+    public void removeArcsGreaterThen(int Weight);
+
+    public int nodeCount();
+
+    public int arcCount();
+
+    public int arcWeightSum();
+
+    public String nodeList();
+
+    public String operationList();
+
+}
+
