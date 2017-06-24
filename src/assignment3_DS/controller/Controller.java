aa3e091ce07e163f2699e358ccd6824c3c88313f
pv42 <pv42.97@gmail.com>
2017-06-25 00:17:07 +0200

    changed model / controller (+jdoc)

--- src/assignment3_DS/controller/Controller.java
+++ src/assignment3_DS/controller/Controller.java
@@ -4,7 +4,7 @@ package assignment3_DS.controller;
  * Created on 23.06.2017.
  */
 public class Controller {
-    static public void removeArcsGreaterThen(int Weight) {
+    static public void removeArcsGreaterThen(int weight) {
         // todo
     }
 
--- src/assignment3_DS/model/Model.java
+++ src/assignment3_DS/model/Model.java
@@ -1,10 +1,12 @@
 package assignment3_DS.model;
 
+import java.util.Observer;
+
 /**
  * Created by pv42 on 24.06.2017.
  */
 public class Model {
-
+    // todo remove static
 
     static public void requestNodeList(){
         //todo
@@ -12,6 +14,10 @@ public class Model {
 
     static public void requestOperationList(){
         //todo
+    }
+
+    public static void registerObservers(Observer arcCountObserver, Observer nodeCountObserver, Observer sumArcWeightObserver) {
+        // todo
     }
 
 }
