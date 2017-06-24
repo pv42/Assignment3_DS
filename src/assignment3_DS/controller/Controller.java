55a1cd1aa2797c560a284689f29f8b591ec5c640
pv42 <pv42.97@gmail.com>
2017-06-25 01:52:55 +0200

    changed model / controller (+jdoc)

--- src/assignment3_DS/controller/Controller.java
+++ src/assignment3_DS/controller/Controller.java
@@ -1,11 +1,16 @@
 package assignment3_DS.controller;
 
+import java.io.File;
+
 /**
  * Created on 23.06.2017.
  */
 public class Controller {
     static public void removeArcsGreaterThen(int weight) {
         // todo
+    }
+    public static void loadGraph(File nodeFile, File arcFile) {
+        //todo
     }
 
 }
