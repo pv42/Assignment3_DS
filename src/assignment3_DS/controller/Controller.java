6197d6e4a0e9f3837143bf9e4b8d282c693acb3a
pv42 <pv42.97@gmail.com>
2017-06-28 21:17:03 +0200

    refractoring, added observable code

--- src/assignment3_DS/controller/Controller.java
+++ src/assignment3_DS/controller/Controller.java
@@ -1,17 +1,65 @@
 package assignment3_DS.controller;
 
+import assignment3_DS.model.Model;
+import assignment3_DS.view.MainWindow;
+import assignment3_DS.view.OpenDialog;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.File;
 
 /**
  * Created on 23.06.2017.
  */
 public class Controller {
+    public Controller(){
+        Model model = new Model();
+        MainWindow mainWindow = new MainWindow(model);
+        ActionListener loadGraphActionListener = new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                OpenDialog.createGUI();
+                //open a window to load a new graph on click
+            }
+        };
+        ActionListener outputNodesActionListener = new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //prints a list of all arcs into the mainText
+                Model.requestNodeList();
+
+            }
+        };
+        ActionListener outputOperationsActionListener = new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //prints a list of all operations into the mainText
+                Model.requestOperationList();
+
+            }
+        };
+        ActionListener removeActionListener = new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // i need to find a solution !
+                try {
+                    Controller.removeArcsLongerThan(Integer.parseInt(mainWindow.getRevoveTextValue()));
+                } catch (NumberFormatException ex) {
+                    JOptionPane.showMessageDialog(null, "Input is not a number", "Error", JOptionPane.ERROR_MESSAGE);
+
+                }
+            }
+        };
+        mainWindow.registerClickListeners(loadGraphActionListener,outputNodesActionListener,outputOperationsActionListener,removeActionListener);
+    }
     static public void removeArcsLongerThan(int weight) {
         // todo
     }
     public static void loadGraph(File nodeFile, File arcFile) {
         //todo
     }
+
 
 }
 
--- src/assignment3_DS/model/Model.java
+++ src/assignment3_DS/model/Model.java
@@ -1,11 +1,27 @@
 package assignment3_DS.model;
 
+import assignment3_DS.graph.Graph;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Observable;
 import java.util.Observer;
 
 /**
  * Created by pv42 on 24.06.2017.
  */
 public class Model {
+    private Graph graph;
+    private Observable arcCountObservable;
+    private Observable nodeCountObserable;
+    private Observable sumArcWeightObserable;
+    private Observable mainTextObservable;
+
+    public Model() {
+        graph = new Graph(new HashMap<>(),new ArrayList<>()); // empty graph
+        arcCountObservable = new Observable();
+        //arcCountObservable.
+    }
     // todo remove static
 
     static public void requestNodeList(){
@@ -16,8 +32,11 @@ public class Model {
         //todo
     }
 
-    public static void registerObservers(Observer arcCountObserver, Observer nodeCountObserver, Observer sumArcWeightObserver, Observer mainTextObserver) {
-        // todo
+    public void registerObservers(Observer arcCountObserver, Observer nodeCountObserver, Observer sumArcWeightObserver, Observer mainTextObserver) {
+        arcCountObservable.addObserver(arcCountObserver);
+        nodeCountObserable.addObserver(nodeCountObserver);
+        sumArcWeightObserable.addObserver(sumArcWeightObserver);
+        mainTextObservable.addObserver(mainTextObserver);
     }
 
 }
